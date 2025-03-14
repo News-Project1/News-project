0b993489b3b92669c71e0eca6af776018d73977d
Deyaa-Abualrub <dyaaabualrub12@gmail.com>
2025-03-15 00:29:43 +0300

    aaa

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 const articleRoutes = require("./routes/articleRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const connectDB = require("./config/db");
+const contactMessage = require('./routes/contactMessageRoutes')
 
 const app = express();
 app.use(express.json());
@@ -22,10 +23,11 @@ app.use(
 connectDB();
 
 app.use('/auth', authRoutes);
+app.use("/api", contactMessage);
 app.use("/api/articles", articleRoutes);
 app.use("/api/categories", categoryRoutes);
 // app.use("/api/journalist", journalistRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
